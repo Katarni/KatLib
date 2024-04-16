976ce865c35a4541def957c5b867229854dcece6
Тимур Ахметзянов <ahmetzianov.temur@ya.ru>
2024-04-16 19:00:36 +0300

    added header file

---
+++ include/KatLib.h
@@ -0,0 +1,17 @@
+//
+// Created by Тимур Ахметзянов on 16.04.2024.
+//
+
+#pragma once
+
+#include "Button.h"
+#include "DialogWin.h"
+#include "Div.h"
+#include "ErrorMessage.h"
+#include "Label.h"
+#include "SelectedItem.h"
+#include "TextInput.h"
+#include "scroll-area/CheckBox.h"
+#include "scroll-area/HorScrollArea.h"
+#include "scroll-area/ScrollAreaElm.h"
+#include "scroll-area/ScrollTextInputs.h"
